8ced2717999f49d6c8c50b2f534e36f4d5e29774
PagarKun <riskiputraantono@gmail.com>
2025-11-24 12:17:10 +0700

    Api Member

---
+++ app/src/main/java/com/example/dna/DashBoardFragment.kt
@@ -0,0 +1,88 @@
+package com.example.dna
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class DashBoardFragment : Fragment() {
+
+    private lateinit var recycleView: RecyclerView
+    private val dataList = ArrayList<MemberRequest>()
+    private lateinit var adapter: AdapterClass
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        Log.d("DEBUG_FLOW ","Fragment created")
+
+        val view = inflater.inflate(R.layout.fragment_dash_board, container, false)
+
+        recycleView = view.findViewById(R.id.recycleViews)
+        recycleView.layoutManager = LinearLayoutManager(requireContext())
+
+        adapter = AdapterClass(dataList) { item ->
+            showBottomSheetDetail(item)
+        }
+        recycleView.adapter = adapter
+        Log.d("DEBUG_FLOW","Calling API......")
+
+        // Ambil API
+        RetrofitClient.instance.getStaff().enqueue(object : Callback<List<MemberRequest>> {
+
+            override fun onResponse(call: Call<List<MemberRequest>>, response: Response<List<MemberRequest>>) {
+                Log.d("DEBUG_FLOW", "API onResponse triggered")
+
+                if (response.isSuccessful) {
+                    val data = response.body() ?: emptyList()
+
+                    Log.d("API_DATA","Data: $data")
+
+                    dataList.clear()
+                    dataList.addAll(data)
+                    adapter.notifyDataSetChanged()
+                } else {
+                    Log.d("API_DATA","Error body: ${response.errorBody()?.string()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<MemberRequest>>, t: Throwable) {
+                Log.d("DEBUG_FLOW","API FAILED: ${t.message}")
+            }
+        })
+
+
+
+        return view
+    }
+
+    fun showBottomSheetDetail(item: MemberRequest) {
+        val bottomSheet = BottomSheetDialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.popup_detail_karyawan, null)
+
+        val img = view.findViewById<ImageView>(R.id.profileDetail)
+        val nama = view.findViewById<TextView>(R.id.namaDetail)
+        val email = view.findViewById<TextView>(R.id.email_detail)
+
+
+        nama.text = item.name
+        email.text = item.username
+
+        bottomSheet.setContentView(view)
+        bottomSheet.behavior.peekHeight = 900
+        bottomSheet.show()
+    }
+}
